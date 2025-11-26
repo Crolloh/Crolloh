62482d94266f2dfb5d33638e52f9d180676cb2af
Crolloh <latrellbang@gmail.com>
2025-11-26 20:01:44 +0800

    day 2 of Qt5

--- GUI_practice.py
+++ GUI_practice.py
@@ -1,5 +1,5 @@
 import sys 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QVBoxLayout, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLineEdit
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
 
@@ -7,9 +7,12 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("weewoopoopoo")
-        self.setGeometry(500, 500, 500, 500)
-        self.ButtonY = QPushButton('Yes', self)
-        self.ButtonN = QPushButton('No', self)
+        self.setGeometry(0, 0, 500, 500)
+        self.ButtonY = QPushButton('Yes')
+        self.ButtonN = QPushButton('No')
+        self.name_of_user = QLineEdit()
+        self.Submit = QPushButton('Submit')
+        self.Greeting = QLabel('Welcome!')
         self.initUIDesign()
 
         ## this is for alignment and adding label ##
@@ -35,14 +38,29 @@ class MainWindow(QMainWindow):
         central_widget = (QWidget())
         self.setCentralWidget(central_widget)
         vbox = QVBoxLayout()
+        hbox_top = QHBoxLayout()
+        self.Greeting.setStyleSheet('color: green' 
+                                    )
+        self.Greeting.setAlignment(Qt.AlignCenter)
+        vbox.addWidget(self.Greeting)
+        self.name_of_user.setStyleSheet('font-size: 20px;')
+        self.name_of_user.setPlaceholderText('What''s your name?')
+        self.Submit.setStyleSheet('font-weight: bold;'
+                                  'color: blue')
+        hbox_top.addWidget(self.name_of_user, stretch=4)
+        hbox_top.addWidget(self.Submit, stretch=1)
+        vbox.addLayout(hbox_top)
+        self.Submit.clicked.connect(self.Submit_on_click)
         self.ButtonY.setStyleSheet('color: white;'
                                    'background-color: green;'
-                                   'font-weight: bold;')
+                                   'font-weight: bold;'
+                                   )
         self.ButtonY.clicked.connect(self.on_click_Yes)
         vbox.addWidget(self.ButtonY)
         self.ButtonN.setStyleSheet('color: white;'
                                    'background-color: red;'
-                                   'font-weight: bold;')
+                                   'font-weight: bold;'
+                                   )
         self.ButtonN.clicked.connect(self.on_click_No)
         vbox.addWidget(self.ButtonN)
         
@@ -54,7 +72,8 @@ class MainWindow(QMainWindow):
         #label2.setStyleSheet('background-color: red;')
         #vbox.addWidget(label2)
         central_widget.setLayout(vbox)
-    
+    def Submit_on_click(self):
+        print('Hi ')
     def on_click_Yes(self):
         print('Yes was clicked!')
         self.ButtonY.setDisabled(True)
